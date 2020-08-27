cfc99be00b9a648405a17150713a1d09b29522c4
elsalaz <andres_974@hotmail.com>
2020-08-27 14:28:41 -0500

    version 5.7

--- js/main3M.js
+++ js/main3M.js
@@ -2,8 +2,8 @@ if (!Detector.webgl) Detector.addGetWebGLMessage();
 
 var containerM, stats, controls;
 var cameraM, sceneM, rendererM, light;
-var ancho = 500;
-var alto = 500;
+let anchoM = 600;
+let altoM = 600;
 var niebla;
 var clock = new THREE.Clock();
 
@@ -13,13 +13,12 @@ init();
 animateM();
 
 function init() {
-  containerM = document.getElementById("my3M");
   Materia = new THREE.Object3D();
 
-  cameraM = new THREE.PerspectiveCamera(45, ancho / alto, 1, 2000);
+  cameraM = new THREE.PerspectiveCamera(45, anchoM / altoM, 1, 2000);
   //camera.lookAt(Materia.position);
   cameraM.position.set(60, 5, 0);
-
+  window.addEventListener("resize", onWindowResizeM, false);
   controls = new THREE.OrbitControls(cameraM);
   controls.target.set(0, 15, 0);
   //controls.minDistance = 70;
@@ -29,6 +28,13 @@ function init() {
   controls.update();
 
   sceneM = new THREE.Scene();
+  rendererM = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  rendererM.setPixelRatio(window.devicePixelRatio);
+  rendererM.setSize(anchoM, altoM);
+  rendererM.shadowMap.enabled = true;
+  containerM = document.getElementById("my3M");
+  containerM.appendChild(rendererM.domElement);
+
   //scene.background = new THREE.Color( 0xa0a0a0 );
   //sceneM.fog = new THREE.Fog(0x3aafa9, 50, 400);
   lightP1 = new THREE.PointLight(0xdd3ef0, 0.35);
@@ -37,16 +43,14 @@ function init() {
   lightP2 = new THREE.PointLight(0x3ef0da, 0.35);
   lightP2.position.set(500, 100, 0);
   sceneM.add(lightP2);
-  lightP3 = new THREE.PointLight(0xdd3ef0, 0.35);
+  lightP3 = new THREE.PointLight(0xdd3ef0, 0.55);
   lightP3.position.set(0, 100, -500);
   sceneM.add(lightP3);
   lightP4 = new THREE.PointLight(0x3ef0da, 0.35);
-
   lightP4.position.set(-500, 300, 500);
   sceneM.add(lightP4);
 
   light = new THREE.HemisphereLight(0xffffff, 0x444444);
-  light.position.set(0, 200, 0);
   sceneM.add(light);
 
   light = new THREE.DirectionalLight(0x3ef0da, 1);
@@ -79,14 +83,14 @@ function init() {
   // model
 
   var loader = new THREE.FBXLoader();
-  loader.load("fbx/Idle.fbx", function(object) {
+  loader.load("fbx/Idle.fbx", function (object) {
     object.mixer = new THREE.AnimationMixer(object);
     mixers.push(object.mixer);
 
     var action = object.mixer.clipAction(object.animations[0]);
     action.play();
 
-    object.traverse(function(child) {
+    object.traverse(function (child) {
       if (child.isMesh) {
         child.castShadow = true;
         child.receiveShadow = true;
@@ -95,37 +99,33 @@ function init() {
 
     //scene.add( object );
     Materia.add(object);
-    Materia.rotation.y = 80 * (Math.PI / 180);
+    Materia.rotation.y = Math.PI / 2;
 
     sceneM.add(Materia);
   });
 
-  rendererM = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-  rendererM.setPixelRatio(window.devicePixelRatio);
-  rendererM.setSize(ancho, alto);
-  rendererM.shadowMap.enabled = true;
-  containerM.appendChild(rendererM.domElement);
-
-  window.addEventListener("resize", onWindowResize, false);
+  //window.addEventListener("resize", onWindowResize, false);
+  animateM();
 }
 
-function onWindowResize() {
-  cameraM.aspect = ancho / alto;
+function onWindowResizeM() {
+  cameraM.aspect = containerM.offsetWidth / containerM.offsetWidth;
   cameraM.updateProjectionMatrix();
 
-  rendererM.setSize(ancho, alto);
+  rendererM.setSize(containerM.offsetWidth, containerM.offsetWidth);
 }
 
 //
 
 function animateM() {
+  onWindowResizeM();
   requestAnimationFrame(animateM);
   if (mixers.length > 0) {
     for (var i = 0; i < mixers.length; i++) {
       mixers[i].update(clock.getDelta());
     }
   }
-
+  //composerM.render();
   rendererM.render(sceneM, cameraM);
 
   //stats.update();
